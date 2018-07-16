1903b1fd7ddc46e0f508d37e85260dd3317129b7
scerreto <scerreto@wellnet.it>
2018-07-16 22:21:45 +0200

    FEATURES-1 Munu - funzioni per la gestione del menu.

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
-console.log("init js!")
+console.log("init js!");
+// variable let and const
+const paragraphs = document.getElementsByTagName("p");
+let numberOfparagrapfs = paragraphs.length;
+// print result
+console.log(paragraphs);
+
+console.log(numberOfparagrapfs);
 
--- app/js/menu.js
+++ app/js/menu.js
@@ -1,1 +1,16 @@
-console.log("menu .js")
+console.log("menu .js");
+
+
+function fetch_text (url) {
+  return fetch(url).then((response) => (response.text()));
+}
+
+function load (page,event) {
+  console.log(page);
+  (event || window.event).preventDefault();
+  fetch_text(`pages/${page}.html`).then((html) => {
+    document.getElementById("main_content").innerHTML = html;
+  }).catch((error) => {
+    console.warn(error);
+  });
+}
